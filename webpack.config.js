e874543be8e0a7b7f0647874e6abf57f3f629394
JunHyeok.Bang <bangvkf3@gmail.com>
2021-10-06 09:43:18 +0900

    webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  target: "web",
+  devServer: {
+    open: true,
+    port: 3001,
+  },
+  devtool: "eval-cheap-source-map",
+};
